525ff8ad3d120d2fe0f4e3e1dc8fe586c27ac08b
孟聪 <767069088@qq.com>
2019-02-21 17:38:54 +0800

    test1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App1" test="smc11"/> -->
+    <smc />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import smc from './components/smc1.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld,
+    smc
   }
 }
 </script>
@@ -22,7 +25,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #56a5f3;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/smc1.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul>
+        <li v-for="(item,index) in items" :key="index">
+            {{index}}-{{item.message}}
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    name:'smc11',
+    data:function(){
+        return {
+            d1:'smc1',
+            d2:'cjj',
+            items:[
+                {message:'Foo'},
+                {message:'Foo1'},
+                {message:'Foo2'},
+                {message:'Foo3'},
+            ]
+        };
+    }
+}
+</script>
